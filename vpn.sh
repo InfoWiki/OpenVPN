be7ab84058a72ee44f60086f4a18f48ce03b1a94
Clément Belloc <will1902@raspberrypi.(none)>
2013-01-08 22:35:08 +0000

    ajout script création client VPN

---
+++ vpn.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#
+#  Création d'un client OpenVPN par C.B pour infowiki.fr
+#  Attention, le script doit être lancé en root !
+#
+echo "Attention, le script doit être lancé en root !"
+echo -n "Tapez le nom du client OpenVPN : "
+read reponse
+
+cd /etc/openvpn/easy-rsa
+
+echo "Creation du client OpenVPN: $REPONSE"
+
+source vars
+./build-key $REPONSE
+
+
+sudo mkdir /etc/openvpn/clientconf/$REPONSE
+sudo cp /etc/openvpn/ca.crt /etc/openvpn/ta.key keys/$REPONSE.crt keys/$REPONSE.key /etc/openvpn/clientconf/$REPONSE
+
+cd /etc/openvpn/clientconf/$REPONSE
+
+cat >> client.conf << EOF
+client
+dev tun
+proto tcp-client
+cipher AES-256-CBC
+resolv-retry infinite
+ca ca.crt
+remote `wget -qO- ifconfig.me/ip` 443
+cert $REPONSE.crt
+key $REPONSE.key
+tls-auth ta.key 1
+nobind
+persist-key
+persist-tun
+comp-lzo
+verb 3
+EOF
+
+sudo cp client.conf client.ovpn
+
+sudo zip $REPONSE.zip *.*
+
+echo "Terminé !"
